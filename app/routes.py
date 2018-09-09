9f09b8f73d1ffe80581f5bbccf8a756089e46806
Adi <u6051965@anu.edu.au>
2018-09-09 13:29:35 +1000

    base changes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
 from app import app
+from flask import render_template, request
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template(
+        'index.html',
+        title = "E-thos Home",
+        year = datetime.now().year,
+    )
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    businessType = request.form['businessType']
+    locationType = request.form['locationType']
+    print(businessType)
+    print(locationType)
+    return render_template(
+        'info.html',
+        title = "E-thos Info",
+        businessVar = businessType,
+        locationVar = locationType,
+        year = datetime.now().year,
+    )
+
+
